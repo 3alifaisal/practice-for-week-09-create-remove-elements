d3b1f4cfd799c33130ea34aaa59909eb75518993
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-03-02 17:57:07 +0100

    finshed
    remove child remove and the template to be added, so basically append to the parent the html

--- create-remove.js
+++ create-remove.js
@@ -9,14 +9,25 @@ add.addEventListener("click", async () => {
 
         /*--------------- Get breed (Hint: Parse from URL) ---------------- */
         // Your code here
-
+        let urlParts = url.split("/");
+        let breedSubDirIndex = urlParts.indexOf("breeds");
+        let newBreedName = urlParts[breedSubDirIndex + 1]
         /*------------ Create new dog card with the url above ------------- */
         /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
         // Your code here
-
+        const newDogCard = document.createElement("li");
+        newDogCard.innerHTML = `
+            <figure>
+                <img src="${url}" >
+                <figcaption>"${newBreedName}"</figcaption>
+            </figure>
+        `;
+  
         /* Add the new dog card as a child to the ul in the .gallery element */
         // Your code here
+        const galleryElement = document.body.querySelector(".gallery > ul");
+        galleryElement.appendChild(newDogCard);
 
     } catch (e) {
         console.log("Couldn't fetch dog :(")
@@ -28,9 +39,11 @@ const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
     /*-------------------- Select the first dog card --------------------- */
     // Your code here
+    const firstDogCard = document.body.querySelector(".gallery > ul > :first-child");
 
     /*-------------------- Remove the first dog card --------------------- */
     // Your code here
+   if(firstDogCard) firstDogCard.remove();
 });
 
 /************************** REMOVE LAST DOG BUTTON ***************************/
@@ -38,7 +51,8 @@ const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
     /*-------------------- Select the last dog card ----------------------- */
     // Your code here
-
+    const lastDogCard = document.body.querySelector(".gallery > ul > :last-child");
     /*-------------------- Remove the last dog card ----------------------- */
     // Your code here
+    if(lastDogCard) lastDogCard.remove();
 });
